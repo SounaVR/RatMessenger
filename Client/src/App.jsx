a49783b61c01157fa261294468f78851f8869a94
SounaVR <pinpoye21@gmail.com>
2023-11-14 11:42:56 +0100

    React Bootstrap

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,17 +1,29 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
+
+// Components
 import Chat from './Pages/Chat';
 import Login from './Pages/Login';
 import Register from './Pages/Register';
+import NavBar from './Components/NavBar';
+
+// CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'react-bootstrap';
 
 function App() {
-    return <>
-        <Routes>
-            <Route path='/' element={ <Chat/> }/>
-            <Route path='/login' element={ <Login/> }/>
-            <Route path='/register' element={ <Register/> }/>
-            <Route path='*' element={ <Navigate to="/" /> }/>
-        </Routes>
-    </>;
+    return (
+        <>
+            <NavBar />
+            <Container>
+                <Routes>
+                    <Route path='/' element={ <Chat/> }/>
+                    <Route path='/login' element={ <Login/> }/>
+                    <Route path='/register' element={ <Register/> }/>
+                    <Route path='*' element={ <Navigate to="/" /> }/>
+                </Routes>
+            </Container>
+        </>
+    );
 }
 
 export default App;

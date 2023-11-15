65d4828c381fa056fe5cc41d2ee2463a1b087de3
SounaVR <pinpoye21@gmail.com>
2023-11-15 22:45:18 +0100

    API routes edited again & Channels routes

--- Server/Routes/app.js
+++ Server/Routes/app.js
@@ -1,6 +1,6 @@
 // Imports
 const express = require('express');
-const { createServer, findUserServers } = require('../Controllers/servers');
+const { createServer, findUserServers, createChannel, findChannel } = require('../Controllers/servers');
 const { createMessage, getMessages } = require('../Controllers/messages');
 const { registration, authentication, findUser } = require('../Controllers/users');
 
@@ -10,14 +10,18 @@ const router = express.Router();
 // Routes users
 router.post('/users/register', registration);
 router.post('/users/login', authentication);
-router.get('/users/find/:userID', findUser);
+router.get('/users/:userID', findUser);
 
 // Routes servers
 router.post('/servers/create', createServer);
-router.get('/servers/find/:userId', findUserServers);
+router.get('/servers/:userId', findUserServers);
+
+// Routes channels
+router.post('/servers/channels/create', createChannel);
+router.get('/servers/channels/:serverId', findChannel);
 
 // Routes messages
-router.post('/messages/create', createMessage);
-router.get('/messages/:channelId', getMessages);
+router.post('/servers/messages/create', createMessage);
+router.get('/servers/channels/messages/:channelId', getMessages);
 
 module.exports = router;

687f06b1277b201fab2b51d35879b58c6d059a85
SounaVR <pinpoye21@gmail.com>
2023-11-15 22:45:57 +0100

    create and find channels API logic

--- Server/Controllers/servers.js
+++ Server/Controllers/servers.js
@@ -52,4 +52,42 @@ const findUserServers = async (req, res) => {
     }
 };
 
-module.exports = { createServer, findUserServers };
+const createChannel = async (req, res) => {
+    const { serverId, channelName, channelType } = req.body;
+
+    try {
+        const newObjectID = new mongoose.Types.ObjectId();
+        const servers = await model.findById(serverId);
+
+        const newChannel = {
+            _id: newObjectID,
+            name: channelName,
+            type: channelType,
+            createdAt: Date.now()
+        };
+
+        servers.channels.push(newChannel)
+
+        const response = await servers.save();
+
+        res.status(200).json(response);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+};
+
+const findChannel = async (req, res) => {
+    const serverId = req.params.serverId;
+
+    try {
+        const servers = await model.findById(serverId);
+
+        res.status(200).json(servers.channels);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+};
+
+module.exports = { createServer, findUserServers, createChannel, findChannel };

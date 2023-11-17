d5dbc03fefc055bf43288de35fe1ddbb0ead8e3d
SounaVR <pinpoye21@gmail.com>
2023-11-17 12:42:57 +0100

    did I mention about how much I hate CSS ?

--- Client/src/Components/app/CreateChannel.jsx
+++ Client/src/Components/app/CreateChannel.jsx
@@ -1,5 +1,5 @@
 import { useState, useContext } from 'react';
-import { Modal, Button, Form } from 'react-bootstrap';
+import { Modal, Button, Form, InputGroup } from 'react-bootstrap';
 import { App } from '../../Context/App';
 
 const CreateChannel = () => {
@@ -9,7 +9,7 @@ const CreateChannel = () => {
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
-  
+
     return (
         <>
             <p
@@ -20,45 +20,52 @@ const CreateChannel = () => {
 
             <Modal show={show} onHide={handleClose}>
                 <Modal.Header closeButton>
-                    <Modal.Title>Create a channel</Modal.Title>
+                    <Modal.Title>Create Channel</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
                     <Form onSubmit={createChannel}>
-                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                            <Form.Label>Channel Type</Form.Label>
-                            {['radio'].map((type) => (
-                                <div key={`reverse-${type}`} className="mb-3">
-                                <Form.Check
-                                    reverse
-                                    label="Text"
-                                    value="Text"
-                                    name="group1"
-                                    type={type}
-                                    onChange={(e) => {
-                                        updateChannelInfo({ ...channelInfo, channelType: e.target.value })
-                                    }}
-                                />
-                                <Form.Check
-                                    reverse
-                                    label="Voice"
-                                    value="Voice"
-                                    name="group1"
-                                    type={type}
-                                    onChange={(e) => {
-                                        updateChannelInfo({ ...channelInfo, channelType: e.target.value })
-                                    }}
-                                />
+                        <Form.Group>
+                            <Form.Label>CHANNEL TYPE</Form.Label>
+                            <div key="channelType" id="channelType">
+                                <div className="radio-option">
+                                    <Form.Check
+                                        defaultChecked
+                                        value="Text"
+                                        name="group1"
+                                        type="radio"
+                                        className="radio-input"
+                                        onChange={(e) => {
+                                            updateChannelInfo({ ...channelInfo, channelType: e.target.value });
+                                        }}
+                                    />
+                                    <span className="radio-label">Text</span>
                                 </div>
-                            ))}
-                            <Form.Label>Channel name</Form.Label>
-                            <Form.Control
-                                type="text"
-                                placeholder="Put a name..."
-                                autoFocus
-                                onChange={(e) =>
-                                    updateChannelInfo({ ...channelInfo, channelName: e.target.value })
-                                }
-                            />
+                                <div className="radio-option">
+                                    <Form.Check
+                                        value="Voice"
+                                        name="group1"
+                                        type="radio"
+                                        className="radio-input"
+                                        onChange={(e) => {
+                                            updateChannelInfo({ ...channelInfo, channelType: e.target.value });
+                                        }}
+                                    />
+                                    <span className="radio-label">Voice</span>
+                                </div>
+                            </div>
+                            <Form.Label>CHANNEL NAME</Form.Label>
+                            <InputGroup>
+                                <InputGroup.Text>#</InputGroup.Text>
+                                <Form.Control
+                                    className='channelName shadow-none'
+                                    type="text"
+                                    placeholder="new-channel"
+                                    autoFocus
+                                    onChange={(e) =>
+                                        updateChannelInfo({ ...channelInfo, channelName: e.target.value })
+                                    }
+                                />
+                            </InputGroup>
                         </Form.Group>
                         
                         <br />

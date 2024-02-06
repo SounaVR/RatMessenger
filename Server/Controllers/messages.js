aa992251e4003394b857b9340d8866dbc3839ca6
Souna <59102661+SounaVR@users.noreply.github.com>
2024-02-06 19:13:04 +0100

    add joi to API to validate stuff, handle when messages is empty and sort them otherwise

--- Server/Controllers/messages.js
+++ Server/Controllers/messages.js
@@ -1,19 +1,33 @@
 const model = require('../Models/messages');
 
+const Joi = require('joi');
+
 const createMessage = async (req, res) => {
     const { channelId, authorId, content } = req.body;
 
-    const message = new model({
-        channelId, authorId, content
+    // Define a schema for validation
+    const schema = Joi.object({
+        channelId: Joi.string().required(),
+        authorId: Joi.string().required(),
+        content: Joi.string().required(),
     });
 
     try {
+        // Validate the request data
+        await schema.validateAsync({ channelId, authorId, content });
+
+        // Create a new message
+        const message = new model({
+            channelId, authorId, content
+        });
+
+        // Save the message to the database
         const response = await message.save();
 
         res.status(200).json(response);
     } catch (err) {
         console.log(err);
-        res.status(500).json(err);
+        res.status(400).json({ error: 'Invalid data' });
     }
 };
 
@@ -21,12 +35,17 @@ const getMessages = async (req, res) => {
     const { channelId } = req.params;
 
     try {
-        const messages = await model.find({ channelId });
+        const messages = await model.find({ channelId: channelId })
+            .sort({ createdAt: -1 })
 
-        res.status(200).json(messages);
+        if (!messages) {
+            res.status(200).json([]);
+        } else {
+            res.status(200).json(messages);
+        }
     } catch (err) {
-        console.log(err);
-        res.status(500).json(err);
+        console.error(err);
+        res.status(500).json({ error: 'Internal server error' });
     }
 };
 

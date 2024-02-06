c9a385e8b4c715da853745bdc3a7fc26a1b0a941
Souna <59102661+SounaVR@users.noreply.github.com>
2024-02-06 19:10:22 +0100

    Sidebars removed and got separated

--- Client/src/Pages/Home.jsx
+++ Client/src/Pages/Home.jsx
@@ -1,47 +1,28 @@
-// Home component
-import { useContext } from "react";
-import { App } from '../Context/App';
-import UserServer from '../Components/app/UserServer';
-import UserChannel from '../Components/app/UserChannel';
-import CreateServer from '../Components/app/CreateServer';
-import CreateChannel from '../Components/app/CreateChannel';
+//Home Components
+import { useState } from 'react';
+import ServerSidebar from '../Components/ServerSidebar';
+import ChannelSidebar from '../Components/ChannelSidebar';
+import Chat from '../Components/Chat';
 
 // CSS
 import '../Styles/home.css';
 
 const Home = () => {
-    const { userServers, isUserServersLoading, channelInfo, updateChannelInfo, userChannels, setIsChannelsNotLoading } = useContext(App);
-
-    const handleServerClick = (serverId) => {
-        updateChannelInfo({ ...channelInfo, serverId: serverId })
-        setIsChannelsNotLoading(true);
-    };
-
+    const [activeChannel, setActiveChannel] = useState(null);
+    
+    const handleChannelChange = (channelId) => {
+        setActiveChannel(channelId)
+    }
+    
     return (
-        <div id="chat-container">
-            {userServers?.length < 0 ? null : (
-                <div className="sidebar">
-                    {isUserServersLoading && <p>Loading servers...</p>}
-                    {userServers?.map((server, i) => (
-                        <UserServer key={i} server={server} handleServerClick={handleServerClick} />
-                    ))}
-                    <CreateServer />
-                </div>
+        <div className="app-container">
+            <ServerSidebar />
+            <ChannelSidebar onChannelChange={handleChannelChange} />
+            {activeChannel ? (
+                <Chat activeChannel={activeChannel} />
+            ) : (
+                <p>Click on a channel first</p>
             )}
-            <div className="main-content">
-                
-                {!userChannels && (
-                    <p>Select or create a server first</p>
-                )}
-                {userChannels && (
-                    <div className="channels-list">
-                        <CreateChannel />
-                        {userChannels?.map((channel, i) => (
-                            <UserChannel key={i} channel={channel} />
-                        ))}
-                    </div>
-                )}
-            </div>
         </div>
     );
 }

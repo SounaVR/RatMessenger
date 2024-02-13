73e3fdda250d1706c9901b258cb322456aa03c7d
Souna <59102661+SounaVR@users.noreply.github.com>
2024-02-13 10:41:39 +0100

    some weird vite stuff

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import reactRefresh from '@vitejs/plugin-react-refresh';;
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    mimeTypes: {
-      '.js': 'application/javascript',
+    plugins: [react(), reactRefresh()],
+    server: {
+        mimeTypes: {
+            '.js': 'application/javascript',
+        },
     },
-  },
-})
+});

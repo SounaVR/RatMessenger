bdc30c484a8de0d7056cb380c738ae99193d9959
Souna <59102661+SounaVR@users.noreply.github.com>
2024-02-13 10:46:17 +0100

    set the active channel for styling

--- Client/src/Components/ChannelSidebar.jsx
+++ Client/src/Components/ChannelSidebar.jsx
@@ -3,7 +3,7 @@ import { App } from '../Context/App';
 import UserChannel from '../Components/app/UserChannel';
 import CreateChannel from '../Components/app/CreateChannel';
 
-const ChannelSidebar = ({ onChannelChange }) => {
+const ChannelSidebar = ({ onChannelChange, activeChannel }) => {
     const { userChannels } = useContext(App);
 
     const handleChannelClick = (channelId) => {
@@ -12,21 +12,19 @@ const ChannelSidebar = ({ onChannelChange }) => {
 
     return (
         <div className="channels-sidebar">
-            {!userChannels && (
-                    <p>Select or create a server first</p>
-                )}
-                {userChannels && (
-                    <div className="channels-list">
-                        <CreateChannel />
-                        {userChannels?.map((channel, i) => (
-                            <UserChannel
-                                key={i}
-                                channel={channel}
-                                onClick={() => handleChannelClick(channel._id)}
-                            />
-                        ))}
-                    </div>
-                )}
+            {userChannels && (
+                <div className="channels-list">
+                    <CreateChannel />
+                    {userChannels?.map((channel, i) => (
+                        <UserChannel
+                            key={i}
+                            channel={channel}
+                            onClick={() => handleChannelClick(channel._id)}
+                            isActive={channel._id === activeChannel}
+                        />
+                    ))}
+                </div>
+            )}
         </div>
     );
 };
